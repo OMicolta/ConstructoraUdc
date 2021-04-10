af179a7e3a485fc545af47806a2f5c835ca8b917
Oivar Micolta <oivarcode@gmail.com>
2021-04-09 22:30:40 -0500

    Agregar archivos de proyecto.

---
+++ ConstructoraUdcModel/DbModel/RoleDbModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructoraUdcModel.DbModel
+{
+    public class RoleDbModel
+    {
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private String name;
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private int removed;
+
+        public int Removed
+        {
+            get { return removed; }
+            set { removed = value; }
+        }
+
+
+
+    }
+}
---
+++ ConstructoraUdcModel/Mappers/GeneralMapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructoraUdcModel.Mappers
+{
+    public abstract class GeneralMapper< T1, T2 >
+    {
+        public abstract T1 MapperT2T1( T2 input );
+
+        public abstract T2 MapperT1T2(T1 input);
+
+        public abstract IEnumerable<T1> MapperT2T1(IEnumerable<T2> input);
+
+        public abstract IEnumerable<T2> MapperT1T2(IEnumerable<T1> input);
+    }
+}
