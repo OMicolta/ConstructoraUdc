030eca6a50b83187b0dc23f9f17fc285616d42ff
Oivar Micolta <oivarcode@gmail.com>
2021-04-10 18:06:16 -0500

    Capa de modelo - Definicion del CRUD de Roles

--- ConstructoraUdcModel/Mappers/GeneralMapper.cs
+++ ConstructoraUdcModel/Mappers/GeneralMapper.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace ConstructoraUdcModel.Mappers
 {
-    public abstract class GeneralMapper< T1, T2 >
-    {
+    public abstract class GeneralMapper< T1, T2 >    {
+        
         public abstract T1 MapperT2T1( T2 input );
 
         public abstract T2 MapperT1T2(T1 input);
